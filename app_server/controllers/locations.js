70eaf0e83c22987888b800131478c41430a65c66
Chris VanHorn <chris_vnhrn@yahoo.com>
2021-02-10 17:54:42 -0600

    ch8 just started

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,97 +1,148 @@
-/* GET 'home' page */
-const homelist = (req, res) => {
-  res.render('locations-list', {
-    title: 'FIDO',
-    pageHeader: {
-      title: 'FIDO',
-      strapline: "can find places for your pet's needs with bigTruck parking!"
-    },
+const request = require('request');
+const apiOptions = {
+  server: 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = 'https://sheltered-gorge-21533.herokuapp.com/';
+}
 
-    sidebar: "Looking for Trucker friendly locations to fill your pet's needs? FIDO will help you to locate a friendly local pet store, dog park, or emergency medical facilities and a bigTruck parking spot at the same time.",
+const formatDistance = (distance) => {
+  let thisDistance = 0;
+  let unit = 'm';
+  if (distance > 1000) {
+    thisDistance = parseFloat(distance / 1000).toFixed(1);
+    unit = 'km';
+  } else {
+    thisDistance = Math.floor(distance);
+  }
+  return thisDistance + unit;
+};
 
-    locations: [{
-      name: 'Petco',
-      address: '123 Bone dr, Big Bowl, Mo 64333',
-      rating: 3,
-      facilities: ['Pet Grooming', 'Veterinarian', 'Pet Motel', 'Truck Parking'],
-      distance: '1.2km'
-    },{
-      name: "Kate's Canine Resort",
-      address: '2823 Main St, Kansas City, MO 64108',
-      rating: 5,
-      facilities: ['Pet Grooming', 'Pet Motel', 'Dog Walking'],
-      distance: '1.6km'
-    },{
-      name: 'KC Pet Food',
-      address: '1725 Southwest Blvd, Kansas City, KS 66103',
-      rating: 4,
-      facilities: ['Everyday Wholesale Pricing', 'Truck Parking'],
-      distance: '3.2km'
-    }]
+const showError = (req, res, status) => {
+  let title = '';
+  let content = '';
+
+  if (status === 404) {
+    title = '404, page not found';
+    content = "Oh dear, Looks like we can't find this page. Sorry";
+  } else {
+    title = `${status}, something's gone wrong`;
+    content = 'Something, somewhere, has gone just a little bit wrong.';
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title,
+    content
   });
 };
 
-/* GET 'Location info' page */
+/* GET 'home' page */
+const renderHomepage = (req, res, responseBody) => {
+  let message = null;
+  if (!(responseBody instanceof Array)) {
+    message = 'API lookup error';
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = 'No places found nearby';
+    }
+  }
+  res.render('locations-list',
+    {
+      title: 'FIDO',
+      pageHeader: {
+        title: 'FIDO',
+        strapLine: "FIDO can find places for your pet's needs with bigTruck parking!"
+      },
+      sidebar: "Looking for Trucker friendly locations to fill your pet's needs? FIDO will help you to locate a friendly local pet store, dog park, or emergency medical facilities and a bigTruck parking spot at the same time.",
+      locations: responseBody,
+      message
+    }
+  );
+};
 
-const locationInfo = (req, res) => {
+const homelist = (req, res) => {
+  const path = '/api/locations';
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {},
+    qs: {
+      lng: -94.58374109230878,
+      lat: 39.06963609230878,
+      maxDistance: 20
+    }
+  };
+  request(
+    requestOptions,
+    (err, {statusCode}, body) => {
+      let data = [];
+      if (statusCode === 200 && body.length) {
+        data = body.map( (item) => {
+          item.distance = formatDistance(item.distance);
+          return item;
+        });
+      }
+      renderHomepage(req, res, data);
+    }
+  );
+};
+
+const renderDetailPage = (req, res, location) => {
   res.render('location-info',
     {
-      title: 'Petco',
+      title: location.name,
        pageHeader: {
-        title: 'Petco',
+        title: location.name,
       },
       sidebar: {
         context: 'is on FIDO because it has a great selection of pet food, toys, and pet services.',
-        callToAction: "If you've been and you like it ~ or if you don't ~ please leave a review to help other people just like you."
+        callToAction: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
       },
-      location: {
-        name: 'Petco',
-        address: '123 Bone dr, Big Bowl, Mo 64333',
-        rating: 3,
-        facilities: ['Pet Food', 'Pet Grooming', 'Veterinarian', 'Dog Trainer'],
-        coords: {lat: 39.06966493333336, lng: -94.5837033369857},
-        openingTimes: [
-          {
-            days: 'Monday - Friday',
-            opening: '8:00am',
-            closing: '7:00pm',
-            closed: false
-          },
-          {
-            days: 'Saturday',
-            opening: '8:00am',
-            closing: '5:00pm',
-            closed: false
-          },
-          {
-            days: 'Sunday',
-            closed: true
-          }
-        ],
-        reviews: [
-          {
-            author: 'George Brett',
-            rating: 5,
-            timestamp: '16 February 2020',
-            reviewText: 'My Dog Loves this place.'
-          },
-          {
-            author: 'Trainer Joe',
-            rating: 3,
-            timestamp: '16 June 2013',
-            reviewText: 'The training here is pretty good. two thumbs up.'
-          }
-        ]
+      location
+    }
+  );
+};
+
+const getLocationInfo = (req, res, callback) => {
+  const path = `/api/locations/${req.params.locationid}`;
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {}
+  };
+  request(
+    requestOptions,
+    (err, {statusCode}, body) => {
+      const data = body;
+      if (statusCode === 200) {
+        data.coords = {
+          lng: body.coords[0],
+          lat: body.coords[1]
+        }
+        callback(req, res, data);
+      } else {
+        showError(req, res, statusCode);
       }
     }
   );
 };
+
+
+
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
+  getLocationInfo(req, res,
+    (req, res, responseData) => renderDetailPage(req, res, responseData)
+  );
+};
+
 /* GET 'Add review' page */
 const addReview = (req, res) => {
   res.render('location-review-form',
     {
-      title: 'Review Starcups on Loc8r' ,
-      pageHeader: { title: 'Review Starcups' }
+      title: 'Review Petco on FIDO',
+      pageHeader: { title: 'Review Petco' }
     }
   );
 };
