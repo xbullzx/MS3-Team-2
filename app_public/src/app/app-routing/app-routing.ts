aeb62494cf1568cca799592080c98899484aa79b
Dishaun <dishaunphillips23@gmail.com>
2021-02-17 21:48:31 -0600

    2-17

---
+++ app_public/src/app/app-routing/app-routing.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from '../about/about.component';
+import { HomepageComponent } from '../homepage/homepage.component';
+import { LocationDetailsComponent } from '../location-details/location-details.component';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+{
+    path: 'coupons',
+    component: LocationDetailsComponent
+ },
+ {
+   path: 'list',
+    component: RegisterComponent
+  },
+  {
+    path: 'comments',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing/app-routing';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { HomeListComponent } from './home-list/home-list.component';
+import { AboutComponent } from './about/about.component';
+import { FrameworkComponent } from './framework/framework.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LocationDetailsComponent } from './location-details/location-details.component';
+import { RegisterComponent } from './register/register.component'
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    HomeListComponent,
+    AboutComponent,
+    FrameworkComponent,
+    HomepageComponent,
+    PageHeaderComponent,
+    LocationDetailsComponent,
+    RegisterComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
